7c73a1d01b7e7e1bed1b7f916092f43af658a2e6
Artem Bobylev <72976030+Gertykhan@users.noreply.github.com>
2022-11-14 00:57:45 +0300

    Third day of work css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: AppComponent
+  }
+];
 
 @NgModule({
+
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+      onSameUrlNavigation: 'reload'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
