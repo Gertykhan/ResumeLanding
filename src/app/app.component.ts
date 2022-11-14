d45dc08918da112cd8b1db12991534aaefdc89d8
Artem Bobylev <72976030+Gertykhan@users.noreply.github.com>
2022-11-14 23:12:56 +0300

    Start coding TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('animationTriggerName', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('1.2s', style({ opacity: 1 })),
+      ]),
+      transition('* => void', [
+        animate('1.2s', style({ opacity: 0 })),
+      ]),
+    ])
+  ]
 })
 export class AppComponent {
   title = 'ResumeLanding';
 
+  table1Color: string = "#FFFFFF";
+  table2Color: string = "#FFFFFF";
+  tester = false;
+
+  switchColor() {
+    this.table1Color = "#343434"
+  }
+  switchBack() {
+    this.table1Color = "#FFFFFF"
+  }
+  switchColor2() {
+    this.table2Color = "#343434"
+  }
+  switchBack2() {
+    this.table2Color = "#FFFFFF"
+  }
+  press(){
+    this.tester = !this.tester;
+  //  todo сделать смещение вниз
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
@@ -18,6 +19,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes,{
       anchorScrolling: 'enabled',
       scrollPositionRestoration: 'enabled',
